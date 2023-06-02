c454f3948b8feef38c37c0ccc4a03db9994fa61e
Gotacme31 <abatizj2000@alumno.ipn.mx>
2023-06-02 08:08:16 -0600

    ayuda

--- src/swaggerOptions.js
+++ src/swaggerOptions.js
@@ -8,7 +8,7 @@ export const options = {
       },
       servers: [
         {
-          url: "https://back-node-production-0895.up.railway.app/",
+          url: "https://back-node-production-0895.up.railway.app/3000",
         },
       ],
     },
